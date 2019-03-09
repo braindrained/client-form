e442bf757f0d62c71c8f52a89b04db4880575f7f
Paolo Senese <paolo.senese@casa.it>
2019-03-09 13:42:41 +0100

    new plus

--- src/childrens/CustomPlusMinus.js
+++ src/childrens/CustomPlusMinus.js
@@ -77,7 +77,7 @@ class CustomPlusMinus extends React.Component<any, any> {
 						className: 'box-shadow noselect'
 					}}>
 						<circle {...{ cx: 15, cy: 15, r: 15, fill: 'rgb(50, 63, 72)' }} />
-						<rect {...{ width: 12.418323, height: 1.5814986, x: 8.792613, y: 14.209251 }} />
+						<rect {...{ width: 10, height: 1.5, x: 10, y: 14 }} />
 					</svg>
 					<div style={{ float: 'left' }}>
 						<input {...{
@@ -100,7 +100,8 @@ class CustomPlusMinus extends React.Component<any, any> {
 						className: 'box-shadow noselect'
 					}}>
 						<circle {...{ cx: 15, cy: 15, r: 15, fill: 'rgb(50, 63, 72)' }} />
-						<path {...{ fill: '#FFF', d: 'm 21.207387,15.79075 h -5.386984 v 5.386984 H 14.179598 V 15.79075 H 8.7926135 V 14.209251 H 14.179598 V 8.8222655 h 1.640805 v 5.3869855 h 5.386983 z' }} />
+						<rect {...{ width: 11, height: 1.5, x: 9.5, y: 14 }} />
+						<rect {...{ width: 1.5, height: 11, x: 14, y: 9.5 }} />
 					</svg>
 				</div>
 				<FieldError {...{ isValid, errorMessage }} />
